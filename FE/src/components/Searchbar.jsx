6b4274bdae9856275d882054ee2dd8ca45132a52
Yusuf <altundalyusuf18@gmail.com>
2023-06-24 12:45:01 +0300

    searchbar and logo in progress

--- FE/src/components/Searchbar.jsx
+++ FE/src/components/Searchbar.jsx
@@ -1,7 +1,8 @@
-import {Typography, AppBar, Box, Toolbar, InputBase, styled, Button, IconButton, MenuItem, FormControl, Select } from '@mui/material';
+import { useState } from 'react';
+import { AppBar, Box, Toolbar, InputBase, styled, Button, IconButton, MenuItem, FormControl, Select } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import MenuIcon from '@mui/icons-material/Menu';
-import { useState } from 'react';
+import {LetterN, LetterT, LetterT1, LetterD, LetterA, LetterT2, LetterA1} from '../assets/Logo';
 
 
 const SearchIconWrapper = styled('div')(({ theme }) => ({
@@ -42,7 +43,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-
 const StyledButton = styled(Button)(({ theme }) => ({
   backgroundColor: '#0059BC',
   color: 'white',
@@ -80,57 +80,54 @@ const StyledToolbar2 = styled(Toolbar)(({theme})=>({
   [theme.breakpoints.up('sm')]: {
     display: 'none',
   },
-
 }))
 
 const StyledForm = styled(FormControl)(() => ({
-  color: 'inherit',
   boxSizing: 'border-box',
   position: 'absolute',
   top: '0',
   right: '0',
-  }));
+}));
 
-  const StyledSelect = styled(Select)(({ theme }) => ({
-      height: '40px',
-      [theme.breakpoints.up('xs')]: {
-        display: 'none',
-      },
-      [theme.breakpoints.up('sm')]: {
-        display: 'block',
-        width: '90px',
-      },
-      [theme.breakpoints.up('md')]: {
-        width: '130px',
-      },
-      [theme.breakpoints.up('lg')]: {
-        width: '180px',
-      },
-      '& .MuiInputBase-input': {
-        paddingTop: '10px',
-        paddingLeft: '20px',
-      },
-    }));
+const StyledSelect = styled(Select)(({ theme }) => ({
+    height: '40px',
+    [theme.breakpoints.up('xs')]: {
+      display: 'none',
+    },
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+      width: '90px',
+    },
+    [theme.breakpoints.up('md')]: {
+      width: '130px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: '180px',
+    },
+    '& .MuiInputBase-input': {
+      paddingTop: '10px',
+      paddingLeft: '20px',
+    },
+}));
 
-    const StyledMenuItem = styled(MenuItem)(() => ({
-      backgroundColor: '#00254F',
-      color: '#FFFFFF',
-      '&:hover': {
-        backgroundColor: '#0059BC',
-      },
-      '&.Mui-selected': {
-        backgroundColor: '#0059BC',
-        color: 'white',
-      },
-      '&.Mui-selected:hover': {
-        backgroundColor: '#0059BC',
-      },
-    }));  
+const StyledMenuItem = styled(MenuItem)(() => ({
+  backgroundColor: '#00254F',
+  color: '#FFFFFF',
+  '&:hover': {
+    backgroundColor: '#0059BC',
+  },
+  '&.Mui-selected': {
+    backgroundColor: '#0059BC',
+  },
+  '&.Mui-selected:hover': {
+    backgroundColor: '#0059BC',
+  },
+}));  
 
-  const StyledDiv = styled('div')(() => ({
-    display: 'flex',
-    position: 'relative',
-    }));
+const StyledDiv = styled('div')(() => ({
+  display: 'flex',
+  position: 'relative',
+}));
 
 
 const Searchbar = () => {
@@ -140,54 +137,68 @@ const Searchbar = () => {
   const handleChange = (event) => {
     setCategory(event.target.value);
   };
+
   return (
     <>
       <Box sx={{ flexGrow: 1, border: 'none' }}>
       <AppBar position="static" color='' sx={{boxShadow: 'none'}}>
         {/* Mobil kısmı */}
       <StyledToolbar2>
-          <Typography
-              variant="h5"
-              noWrap
-              component="div" 
-            >
-              NTT Data
-          </Typography>
+        <Box
+        sx={{display: {
+          xs: 'flex',
+        },}}
+        >
+          <div style={{marginRight:'5px'}}>
+            <LetterN/>
+            <LetterT/>
+            <LetterT1/>
+          </div>
+          <div>
+            <LetterD/>
+            <LetterA/>
+            <LetterT2/>
+            <LetterA1/>
+          </div>
+        </Box>
 
           
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon /> 
-            </IconButton> 
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon /> 
+        </IconButton> 
       </StyledToolbar2>
 
     {/* Tablet ve Bilgisayar */}
         <StyledToolbar>
-          <Typography
-            variant="h5"
-            noWrap
-            component="div"
-            sx={{
-              display: {
+          {/* Logo */}
+          <Box sx={{display: {
                 xs: 'none',
-                sm: 'block',
+                sm: 'flex',
               },
               marginRight: {
-                // xs: 2, // theme.breakpoints.up('xs')
                 sm: 3, // theme.breakpoints.up('sm')
                 md: 3, // theme.breakpoints.up('md')
                 lg: 4, // theme.breakpoints.up('lg')
                 xl: 4, // theme.breakpoints.up('xl')
-              },
-            }}  
-          >
-            NTT Data
-          </Typography>
+              },}}>
+          <div style={{marginRight:'5px'}}>
+            <LetterN/>
+            <LetterT/>
+            <LetterT1/>
+          </div>
+          <div>
+            <LetterD/>
+            <LetterA/>
+            <LetterT2/>
+            <LetterA1/>
+          </div>
+          </Box>
 
             {/* Search Bar ve kategori select-option */}
           <StyledDiv >
@@ -195,23 +206,29 @@ const Searchbar = () => {
               placeholder="Search…"
               inputProps={{ 'aria-label': 'search' }}
             /> 
-                <StyledForm variant='filled'>
+                <StyledForm variant={category == ''?'filled':'outlined'}>
                   <StyledSelect
                     value={category}
                     onChange={handleChange}
                     displayEmpty
                     inputProps={{ 'aria-label': 'Without label' }}
                   >
-                    <StyledMenuItem value="">
+                    <StyledMenuItem
+                      sx={{color: 'white',
+                      '&.Mui-selected': {
+                        color: 'black',
+                      },}}
+                      value="">
                       <em>Categories</em>
                     </StyledMenuItem>
-                    <StyledMenuItem value={10}>Ten</StyledMenuItem>
-                    <StyledMenuItem value={20}>Twenty</StyledMenuItem>
-                    <StyledMenuItem value={30}>Thirty</StyledMenuItem>
+                    <StyledMenuItem value={'Ten'}>Ten</StyledMenuItem>
+                    <StyledMenuItem value={'Twenty'}>Twenty</StyledMenuItem>
+                    <StyledMenuItem value={'Thirty'}>Thirty</StyledMenuItem>
                   </StyledSelect>
                 </StyledForm>
-              </StyledDiv>
+          </StyledDiv>
 
+          {/* Arama butonu */}
           <StyledButton>
               <SearchIconWrapper>
                 <SearchIcon />
